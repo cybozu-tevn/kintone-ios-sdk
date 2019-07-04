4974eddf389f3ff9962d19989236ba6cb735f17a
Tran Kim Chau <chau-tran@cybozu.vn>
2019-07-04 15:34:28 +0900

    TEDO-59: Review and update from review points | GetApp

--- kintone-ios-sdkTests/test_scripts/module/app/GetAppTest.swift
+++ kintone-ios-sdkTests/test_scripts/module/app/GetAppTest.swift
@@ -10,71 +10,62 @@ import Nimble
 
 class GetAppTest: QuickSpec {
     override func spec() {
-        let app = App(TestCommonHandling.createConnection())
-        let appName = "App Name"
-        var appId: Int?
-        
-        beforeSuite {
-            print("=== TEST PREPARATION ===")
-            appId = AppUtils.createApp(appModule: app, appName: appName)
-        }
-        
-        afterSuite {
-            print("=== TEST CLEANING UP ===")
-            AppUtils.deleteApp(appId: appId!)
-        }
+        let appModule = App(TestCommonHandling.createConnection())
+        let appInfo = TestConstant.InitData.apps.appInSpace[0]
+        let appId = Int(appInfo.appId)!
         
         describe("GetApp") {
-            
             it("Test_003_Success_ApiToken") {
-                let apiToken = AppUtils.generateApiToken(app, appId!)
-                let tokenPermission  = TokenEntity(tokenString: apiToken, viewRecord: true, addRecord: true, editRecord: true, deleteRecord: true, editApp: true)
-                AppUtils.updateTokenPermission(appModule: app, appId: appId!, token: tokenPermission)
+                let appModuleApiToken = App(TestCommonHandling.createConnection(String(appInfo.apiToken.fullPermission)))
+                let getAppRsp = TestCommonHandling.awaitAsync(appModuleApiToken.getApp(appId)) as! AppModel
                 
-                let appModule = App(TestCommonHandling.createConnection(apiToken))
-                let getAppRsp = TestCommonHandling.awaitAsync(appModule.getApp(appId!)) as! AppModel
-                
-                expect(getAppRsp.getAppId()).to(equal(appId))
-                expect(getAppRsp.getName()).to(equal(appName))
-                expect(getAppRsp.getCode()).to(equal(""))
-                expect(getAppRsp.getCreator()?.getName()).to(equal(TestConstant.Connection.CRED_ADMIN_USERNAME))
-                expect(getAppRsp.getSpaceId()).to(beNil())
-                expect(getAppRsp.getThreadId()).to(beNil())
+                expect(getAppRsp.getAppId()).to(equal(Int(appInfo.appId)!))
+                expect(getAppRsp.getName()).to(equal(appInfo.name))
+                expect(getAppRsp.getCode()).to(equal(appInfo.code))
+                expect(getAppRsp.getDescription()).to(equal(appInfo.description))
+                expect(getAppRsp.getCreator()?.getName()).to(equal(appInfo.creator.name))
+                expect(getAppRsp.getSpaceId()).to(equal(Int(appInfo.spaceId)))
+                expect(getAppRsp.getThreadId()).to(equal(Int(appInfo.threadId)))
             }
             
             it("Test_004_Success") {
-                let getAppRsp = TestCommonHandling.awaitAsync(app.getApp(appId!)) as! AppModel
-                expect(getAppRsp.getAppId()).to(equal(appId))
-                expect(getAppRsp.getName()).to(equal(appName))
-                expect(getAppRsp.getCode()).to(equal(""))
-                expect(getAppRsp.getCreator()?.getName()).to(equal(TestConstant.Connection.CRED_ADMIN_USERNAME))
-                expect(getAppRsp.getSpaceId()).to(beNil())
-                expect(getAppRsp.getThreadId()).to(beNil())
+                let getAppRsp = TestCommonHandling.awaitAsync(appModule.getApp(appId)) as! AppModel
+                
+                expect(getAppRsp.getAppId()).to(equal(Int(appInfo.appId)!))
+                expect(getAppRsp.getName()).to(equal(appInfo.name))
+                expect(getAppRsp.getCode()).to(equal(appInfo.code))
+                expect(getAppRsp.getDescription()).to(equal(appInfo.description))
+                expect(getAppRsp.getCreator()?.getName()).to(equal(appInfo.creator.name))
+                expect(getAppRsp.getSpaceId()).to(equal(Int(appInfo.spaceId)))
+                expect(getAppRsp.getThreadId()).to(equal(Int(appInfo.threadId)))
             }
             
-            it("Test_004_Success_GuesSpaceApp") {
-                // Test setting up
-                let guestAppModule = App(TestCommonHandling.createConnection(TestConstant.Connection.CRED_ADMIN_USERNAME, TestConstant.Connection.CRED_ADMIN_PASSWORD, TestConstant.InitData.GUEST_SPACE_ID!))
-                let guestAppId = AppUtils.createApp(appModule: guestAppModule, appName: appName, spaceId: TestConstant.InitData.GUEST_SPACE_ID, threadId: TestConstant.InitData.GUEST_SPACE_THREAD_ID)
+            it("Test_004_Success_GuestSpace") {
+                let guestSpaceAppInfo = TestConstant.InitData.apps.appInGuestSpace[0]
+                let guestSpaceAppId = Int(guestSpaceAppInfo.appId)!
+                let appModuleGuestSpace = App(TestCommonHandling.createConnection(
+                    TestConstant.Connection.CRED_ADMIN_USERNAME,
+                    TestConstant.Connection.CRED_ADMIN_PASSWORD,
+                    TestConstant.InitData.GUEST_SPACE_ID!))
                 
-                let getAppRsp = TestCommonHandling.awaitAsync(guestAppModule.getApp(guestAppId)) as! AppModel
-                expect(getAppRsp.getAppId()).to(equal(guestAppId))
-                expect(getAppRsp.getName()).to(equal(appName))
-                expect(getAppRsp.getCode()).to(equal(""))
-                expect(getAppRsp.getCreator()?.getName()).to(equal(TestConstant.Connection.CRED_ADMIN_USERNAME))
-                expect(getAppRsp.getSpaceId()).to(equal(TestConstant.InitData.GUEST_SPACE_ID))
-                expect(getAppRsp.getThreadId()).to(equal(TestConstant.InitData.GUEST_SPACE_THREAD_ID))
-                
-                // Test cleaning up
-                AppUtils.deleteApp(appId: guestAppId)
+                let getAppRsp = TestCommonHandling.awaitAsync(appModuleGuestSpace.getApp(guestSpaceAppId)) as! AppModel
+                expect(getAppRsp.getAppId()).to(equal(guestSpaceAppId))
+                expect(getAppRsp.getName()).to(equal(guestSpaceAppInfo.name))
+                expect(getAppRsp.getCode()).to(equal(guestSpaceAppInfo.code))
+                expect(getAppRsp.getDescription()).to(equal(guestSpaceAppInfo.description))
+                expect(getAppRsp.getCreator()?.getName()).to(equal(guestSpaceAppInfo.creator.name))
+                expect(getAppRsp.getSpaceId()).to(equal(Int(guestSpaceAppInfo.spaceId)))
+                expect(getAppRsp.getThreadId()).to(equal(Int(guestSpaceAppInfo.threadId)))
             }
             
             it("Test_005_Error_InvalidAppId") {
                 let invalidAppId = 9999 // non-existed appId
-                let getAppRsp = TestCommonHandling.awaitAsync(app.getApp(invalidAppId)) as! KintoneAPIException
-                var expectedError = KintoneErrorParser.NONEXISTENT_APP_ID_ERROR()
-                expectedError?.replaceMessage(oldTemplate: "%VARIABLE", newTemplate: String(invalidAppId))
-                TestCommonHandling.compareError(getAppRsp.getErrorResponse()!, expectedError!)
+                let result = TestCommonHandling.awaitAsync(appModule.getApp(invalidAppId)) as! KintoneAPIException
+                
+                let actualError = result.getErrorResponse()
+                var expectedError = KintoneErrorParser.NONEXISTENT_APP_ID_ERROR()!
+                expectedError.replaceMessage(oldTemplate: "%VARIABLE", newTemplate: String(invalidAppId))
+                TestCommonHandling.compareError(actualError, expectedError)
             }
         }
     }
