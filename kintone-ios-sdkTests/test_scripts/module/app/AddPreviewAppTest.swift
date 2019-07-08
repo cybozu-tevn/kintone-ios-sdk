953ce186cbf93b17d91374615b8c9ea5989bf58a
Vu Tran <vutranshield@gmail.com>
2019-07-08 16:54:08 +0700

    [AddPreviewAppTest] Refactored test scripts based on reviewing comments

--- kintone-ios-sdkTests/test_scripts/module/app/AddPreviewAppTest.swift
+++ kintone-ios-sdkTests/test_scripts/module/app/AddPreviewAppTest.swift
@@ -12,7 +12,7 @@ class AddPreviewAppTest: QuickSpec {
     override func spec() {
         describe("AddPreviewApp") {
             let app = App(TestCommonHandling.createConnection())
-            let appName = "App Name"
+            let appName = DataRandomization.generateString(length: 8)
             var appIds = [Int]()
             
             afterSuite {
@@ -59,24 +59,53 @@ class AddPreviewAppTest: QuickSpec {
                 appIds.append(appId)
                 let apiToken = AppUtils.generateApiToken(app, appId)
                 let appModuleApiToken = App(TestCommonHandling.createConnection(apiToken))
+                
                 let addPreviewAppRsp = TestCommonHandling.awaitAsync(appModuleApiToken.addPreviewApp(appName)) as! KintoneAPIException
-                TestCommonHandling.compareError(addPreviewAppRsp.getErrorResponse(), KintoneErrorParser.API_TOKEN_ERROR()!)
+                
+                let actualCode = addPreviewAppRsp.getErrorResponse()?.getCode()
+                let actualMessage = addPreviewAppRsp.getErrorResponse()?.getMessage()
+                let expectedCode = KintoneErrorParser.API_TOKEN_ERROR()?.getCode()
+                let expectedMessage = KintoneErrorParser.API_TOKEN_ERROR()?.getMessage()
+                
+                expect(actualCode).to(equal(expectedCode))
+                expect(actualMessage).to(equal(expectedMessage))
             }
             
             // Xcode will catch this error
-            //            fit("Test_093_FailedWithoutAppName") {
+            //            fit("Test_093_Error_WithoutAppName") {
             //                let result = TestCommonHandling.awaitAsync(app.addPreviewApp()) as! KintoneAPIException
             //            }
             
             it("Test_094_Error_MoreThan64CharsAppName") {
                 let invalidAppName = DataRandomization.generateString(length: 65)
+                
                 let addPreviewAppRsp = TestCommonHandling.awaitAsync(app.addPreviewApp(invalidAppName)) as! KintoneAPIException
-                TestCommonHandling.compareError(addPreviewAppRsp.getErrorResponse(), KintoneErrorParser.NAME_LARGER_THAN_64_CHARACTERS_ERROR()!)
+                
+                let actualCode = addPreviewAppRsp.getErrorResponse()?.getCode()
+                let actualMessage = addPreviewAppRsp.getErrorResponse()?.getMessage()
+                let actualErrors = addPreviewAppRsp.getErrorResponse()?.getErrors()
+                let expectedCode = KintoneErrorParser.NAME_LARGER_THAN_64_CHARACTERS_ERROR()?.getCode()
+                let expectedMessage = KintoneErrorParser.NAME_LARGER_THAN_64_CHARACTERS_ERROR()?.getMessage()
+                let expectedErrors = KintoneErrorParser.NAME_LARGER_THAN_64_CHARACTERS_ERROR()?.getErrors()
+                
+                expect(actualCode).to(equal(expectedCode))
+                expect(actualMessage).to(equal(expectedMessage))
+                expect(actualErrors).to(equal(expectedErrors))
             }
             
             it("Test_095_Error_WithoutSpaceId") {
                 let addPreviewAppRsp = TestCommonHandling.awaitAsync(app.addPreviewApp(appName, nil, TestConstant.InitData.SPACE_ID)) as! KintoneAPIException
-                TestCommonHandling.compareError(addPreviewAppRsp.getErrorResponse(), KintoneErrorParser.MISSING_SPACE_ERROR()!)
+                
+                let actualCode = addPreviewAppRsp.getErrorResponse()?.getCode()
+                let actualMessage = addPreviewAppRsp.getErrorResponse()?.getMessage()
+                let actualErrors = addPreviewAppRsp.getErrorResponse()?.getErrors()
+                let expectedCode = KintoneErrorParser.MISSING_SPACE_ERROR()?.getCode()
+                let expectedMessage = KintoneErrorParser.MISSING_SPACE_ERROR()?.getMessage()
+                let expectedErrors = KintoneErrorParser.MISSING_SPACE_ERROR()?.getErrors()
+                
+                expect(actualCode).to(equal(expectedCode))
+                expect(actualMessage).to(equal(expectedMessage))
+                expect(actualErrors).to(equal(expectedErrors))
             }
             
             it("Test_096_Error_NonExistentSpaceId") {
@@ -84,19 +113,46 @@ class AddPreviewAppTest: QuickSpec {
                 formatter.numberStyle = .decimal
                 
                 let addPreviewAppRsp = TestCommonHandling.awaitAsync(app.addPreviewApp(appName, TestConstant.Common.MAX_VALUE, TestConstant.InitData.SPACE_ID)) as! KintoneAPIException
-                var expectedResult = KintoneErrorParser.NONEXISTENT_SPACE_ERROR()!
-                expectedResult.replaceMessage(oldTemplate: "%VARIABLE", newTemplate: formatter.string(for: TestConstant.Common.MAX_VALUE)!)
-                TestCommonHandling.compareError(addPreviewAppRsp.getErrorResponse(), expectedResult)
+                
+                let actualCode = addPreviewAppRsp.getErrorResponse()?.getCode()
+                let actualMessage = addPreviewAppRsp.getErrorResponse()?.getMessage()
+                
+                let expectedCode = KintoneErrorParser.NONEXISTENT_SPACE_ERROR()?.getCode()
+                var expectedMessage = KintoneErrorParser.NONEXISTENT_SPACE_ERROR()?.getMessage()
+                expectedMessage = expectedMessage?.replacingOccurrences(of: "%VARIABLE", with: formatter.string(for: TestConstant.Common.MAX_VALUE)!)
+                
+                expect(actualCode).to(equal(expectedCode))
+                expect(actualMessage).to(equal(expectedMessage))
             }
             
             it("Test_097_Error_NegativeSpaceId") {
                 let addPreViewAppRsp = TestCommonHandling.awaitAsync(app.addPreviewApp(appName, -1, TestConstant.InitData.SPACE_ID)) as! KintoneAPIException
-                TestCommonHandling.compareError(addPreViewAppRsp.getErrorResponse(), KintoneErrorParser.NEGATIVE_SPACE_ERROR()!)
+                
+                let actualCode = addPreViewAppRsp.getErrorResponse()?.getCode()
+                let actualMessage = addPreViewAppRsp.getErrorResponse()?.getMessage()
+                let actualErrors = addPreViewAppRsp.getErrorResponse()?.getErrors()
+                let expectedCode = KintoneErrorParser.NEGATIVE_SPACE_ERROR()?.getCode()
+                let expectedMessage = KintoneErrorParser.NEGATIVE_SPACE_ERROR()?.getMessage()
+                let expectedErrors = KintoneErrorParser.NEGATIVE_SPACE_ERROR()?.getErrors()
+                
+                expect(actualCode).to(equal(expectedCode))
+                expect(actualMessage).to(equal(expectedMessage))
+                expect(actualErrors).to(equal(expectedErrors))
             }
             
-            it("Test_098_Error_WithoutThreadId(") {
+            it("Test_098_Error_WithoutThreadId") {
                 let addPreviewAppRsp = TestCommonHandling.awaitAsync(app.addPreviewApp(appName, TestConstant.InitData.SPACE_ID, nil)) as! KintoneAPIException
-                TestCommonHandling.compareError(addPreviewAppRsp.getErrorResponse(), KintoneErrorParser.MISSING_THREAD_ERROR()!)
+                
+                let actualCode = addPreviewAppRsp.getErrorResponse()?.getCode()
+                let actualMessage = addPreviewAppRsp.getErrorResponse()?.getMessage()
+                let actualErrors = addPreviewAppRsp.getErrorResponse()?.getErrors()
+                let expectedCode = KintoneErrorParser.MISSING_THREAD_ERROR()?.getCode()
+                let expectedMessage = KintoneErrorParser.MISSING_THREAD_ERROR()?.getMessage()
+                let expectedErrors = KintoneErrorParser.MISSING_THREAD_ERROR()?.getErrors()
+                
+                expect(actualCode).to(equal(expectedCode))
+                expect(actualMessage).to(equal(expectedMessage))
+                expect(actualErrors).to(equal(expectedErrors))
             }
             
             it("Test_099_Error_NonExistentThreadId") {
@@ -104,14 +160,30 @@ class AddPreviewAppTest: QuickSpec {
                 formatter.numberStyle = .decimal
                 
                 let addPreviewAppRsp = TestCommonHandling.awaitAsync(app.addPreviewApp(appName, TestConstant.InitData.SPACE_ID, TestConstant.Common.MAX_VALUE)) as! KintoneAPIException
-                var expectedResult = KintoneErrorParser.NONEXISTENT_THREAD_ERROR()!
-                expectedResult.replaceMessage(oldTemplate: "%VARIABLE", newTemplate: formatter.string(for: TestConstant.Common.MAX_VALUE)!)
-                TestCommonHandling.compareError(addPreviewAppRsp.getErrorResponse(), expectedResult)
+                
+                let actualCode = addPreviewAppRsp.getErrorResponse()?.getCode()
+                let actualMessage = addPreviewAppRsp.getErrorResponse()?.getMessage()
+                let expectedCode = KintoneErrorParser.NONEXISTENT_THREAD_ERROR()?.getCode()
+                var expectedMessage = KintoneErrorParser.NONEXISTENT_THREAD_ERROR()?.getMessage()
+                expectedMessage = expectedMessage?.replacingOccurrences(of: "%VARIABLE", with: formatter.string(for: TestConstant.Common.MAX_VALUE)!)
+                
+                expect(actualCode).to(equal(expectedCode))
+                expect(actualMessage).to(equal(expectedMessage))
             }
             
             it("Test_100_Error_NegativeThreadId") {
                 let addPreviewAppRsp = TestCommonHandling.awaitAsync(app.addPreviewApp(appName, TestConstant.InitData.SPACE_ID, -1)) as! KintoneAPIException
-                TestCommonHandling.compareError(addPreviewAppRsp.getErrorResponse(), KintoneErrorParser.NEGATIVE_THREAD_ERROR()!)
+                
+                let actualCode = addPreviewAppRsp.getErrorResponse()?.getCode()
+                let actualMessage = addPreviewAppRsp.getErrorResponse()?.getMessage()
+                let actualErrors = addPreviewAppRsp.getErrorResponse()?.getErrors()
+                let expectedCode = KintoneErrorParser.NEGATIVE_THREAD_ERROR()?.getCode()
+                let expectedMessage = KintoneErrorParser.NEGATIVE_THREAD_ERROR()?.getMessage()
+                let expectedErrors = KintoneErrorParser.NEGATIVE_THREAD_ERROR()?.getErrors()
+                
+                expect(actualCode).to(equal(expectedCode))
+                expect(actualMessage).to(equal(expectedMessage))
+                expect(actualErrors).to(equal(expectedErrors))
             }
             
             // This case needs to setup a user with no previledge to create app
@@ -120,8 +192,16 @@ class AddPreviewAppTest: QuickSpec {
                 let appModule = App(TestCommonHandling.createConnection(
                     TestConstant.Connection.CRED_USERNAME_WITHOUT_CREATE_APP_PERMISSION,
                     TestConstant.Connection.CRED_PASSWORD_WITHOUT_CREATE_APP_PERMISSION))
+                
                 let addPreviewAppRsp = TestCommonHandling.awaitAsync(appModule.addPreviewApp(appName)) as! KintoneAPIException
-                TestCommonHandling.compareError(addPreviewAppRsp.getErrorResponse(), KintoneErrorParser.PERMISSION_ERROR()!)
+                
+                let actualCode = addPreviewAppRsp.getErrorResponse()?.getCode()
+                let actualMessage = addPreviewAppRsp.getErrorResponse()?.getMessage()
+                let expectedCode = KintoneErrorParser.PERMISSION_ERROR()?.getCode()
+                let expectedMessage = KintoneErrorParser.PERMISSION_ERROR()?.getMessage()
+                
+                expect(actualCode).to(equal(expectedCode))
+                expect(actualMessage).to(equal(expectedMessage))
             }
         }
     }
